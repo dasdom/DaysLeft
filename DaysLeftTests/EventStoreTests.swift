997061c33066b0012666406853d1408ab6d4f601
Dominik Hauser <dom@dasdom.de>
2021-08-17 17:45:07 +0200

    Add tests for removing event

--- DaysLeftTests/EventStoreTests.swift
+++ DaysLeftTests/EventStoreTests.swift
@@ -7,15 +7,37 @@ import XCTest
 
 class EventStoreTests: XCTestCase {
 
+  var sut: EventStore!
+
+  override func setUpWithError() throws {
+    sut = EventStore(eventsSerialiser: EventsPersistenceHandlerProtocolDummy())
+  }
+
+  override func tearDownWithError() throws {
+    sut = nil
+  }
+
   func test_add_shouldAddEventToEvents() {
     // arrange
-    let store = EventStore(eventsSerialiser: EventsPersistenceHandlerProtocolDummy())
 
     // act
     let event = Event(name: "Dummy", date: Date())
-    store.add(event)
+    sut.add(event)
 
     // assert
-    XCTAssertEqual(store.events.first, event)
+    XCTAssertEqual(sut.events.first, event)
+  }
+
+  func test_remove_shouldRemoveEventFromEvents() {
+    // arrange
+    let event = Event(name: "Dummy", date: Date())
+    sut.add(event)
+    XCTAssertEqual(sut.events.count, 1)
+
+    // act
+    sut.remove(event: event)
+
+    // assert
+    XCTAssertEqual(sut.events.count, 0)
   }
 }
